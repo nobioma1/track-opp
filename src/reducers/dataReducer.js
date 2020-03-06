a0da5ff338d2fc591046af8d33817c8c77a88047
Noble Obioma <nobioma1@gmail.com>
2020-03-06 20:47:30 +0100

    Setup react redux

---
+++ src/reducers/dataReducer.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {};
+
+const dataReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default dataReducer;
