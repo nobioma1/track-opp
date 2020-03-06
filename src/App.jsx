01512b86ceb58135e8c23f181a98420fd84aef9a
Noble Obioma <nobioma1@gmail.com>
2020-03-07 00:52:52 +0100

    Add Router and refactor to hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-import NewEntry from './components/NewEntry';
+import Header from './components/Header';
 import Dashboard from './components/Dashboard';
+import Applications from './components/Applications';
+import { getApplications } from './actions/data';
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getApplications());
+  }, [dispatch]);
+
   return (
-    <React.Fragment>
-      <div className="fixed w-full bg-blue-500 p-2">
-        <h1 className="text-2xl font-extrabold text-white">Track-Opp</h1>
-      </div>
+    <BrowserRouter>
+      <Header />
       <div className="container mx-auto pt-16 pb-8 px-3">
-        <div className="flex flex-col">
-          <Dashboard />
-          <NewEntry />
-        </div>
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/applications" component={Applications} />
       </div>
-    </React.Fragment>
+    </BrowserRouter>
   );
 };
 
