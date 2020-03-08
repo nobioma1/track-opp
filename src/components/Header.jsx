5c61a0a7e01c3e72de73947dfada55998a616b10
Noble Obioma <nobioma1@gmail.com>
2020-03-08 23:35:53 +0100

    Add logout button to header and conditionally render

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import pwrbtn from '../assets/pwrbtn.svg';
+import { Logout } from '../actions/auth';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(({ authReducer }) => authReducer.user);
+
   return (
     <div className="fixed z-40 w-full bg-blue-500 py-2 px-2 lg:px-6 flex items-center justify-between">
       <Link to="/" className="text-2xl font-extrabold text-white">
         TrackOpp
       </Link>
-      <Link
-        to="/applications"
-        className="text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white lg:mt-0"
-      >
-        Applications
-      </Link>
+      {user && (
+        <div className="flex items-center">
+          <Link
+            to="/applications"
+            className="text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white lg:mt-0"
+          >
+            Applications
+          </Link>
+          <div
+            className="w-6 opacity-2 cursor-pointer ml-2"
+            onClick={() => dispatch(Logout())}
+          >
+            <img className="w-full" src={pwrbtn} alt="logout button" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
