5b8d414136fbaf87fea40c17b6d09e0a09c99c84
Noble Obioma <nobioma1@gmail.com>
2020-03-08 23:38:21 +0100

    Create login component

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+import { firebaseUIConfig, firebaseAuth } from '../../config/firebase';
+import interview from '../../assets/interview.svg';
+
+const Login = () => {
+  const user = useSelector(({ authReducer }) => authReducer.user);
+
+  return (
+    <div
+      className="flex flex-col text-center md:justify-center"
+      style={{ height: '85vh' }}
+    >
+      {!user ? (
+        <div className="flex flex-col items-center">
+          <h1 className="text-3xl font-medium font-mono">
+            Keep track your job applications progress 😎...
+          </h1>
+          <img
+            className="h-full w-2/3 md:w-2/4 opacity-30"
+            src={interview}
+            alt="Interview"
+          />
+          <StyledFirebaseAuth
+            uiConfig={firebaseUIConfig}
+            firebaseAuth={firebaseAuth()}
+          />
+        </div>
+      ) : (
+        <Redirect to="/" />
+      )}
+    </div>
+  );
+};
+
+export default Login;
