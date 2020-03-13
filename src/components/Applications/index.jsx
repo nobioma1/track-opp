1506801fa0df3b81c533e4967313ae01bca80711
Noble Obioma <nobioma1@gmail.com>
2020-03-13 23:07:58 +0100

    Remove tabs sorting for components, add search component

--- src/components/Applications/index.jsx
+++ src/components/Applications/index.jsx
@@ -1,46 +1,35 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
-import ApplicationsNav from './ApplicationsNav';
-import { setCurrentStage, deleteApplication } from '../../actions/applications';
 import ApplicationItem from './ApplicationItem';
+import ApplicationsNav from './ApplicationsNav';
+import Search from '../Search';
+import { setCurrentStage, deleteApplication } from '../../actions/applications';
 import nodata from '../../assets/nodata.svg';
+import { filterApplications } from './filterApplications';
 
 const Applications = () => {
   const [active, setActive] = useState('jobsApplied');
+  const [searchParam, setSearchParam] = useState('');
+
   const { applications, counts } = useSelector(({ applicationsReducer }) => ({
     counts: applicationsReducer.counts,
     applications: applicationsReducer.applications,
   }));
+
   const dispatch = useDispatch();
 
-  // Filtering different tabs
-  const filteredApplications = applications.filter(application => {
-    switch (active) {
-      case 'interviews':
-        return application.interview;
-      case 'offers':
-        return application.offer;
-      case 'noResponse':
-        return (
-          !application.hired &&
-          !application.concluded &&
-          !application.interview &&
-          !application.offer
-        );
-      case 'notAMatch':
-        return application.concluded && !application.hired;
-      default:
-        return true;
-    }
-  });
+  // Filtering different tabs and search param
+  const filteredApplications = filterApplications(
+    applications,
+    active,
+    searchParam
+  );
 
   const updateCurrentStage = (id, value) => {
     dispatch(setCurrentStage(id, value));
   };
-
-  const onEditHandler = id => {};
 
   const onDeleteHandler = id => {
     dispatch(deleteApplication(id));
@@ -59,14 +48,14 @@ const Applications = () => {
         selectTab={setActive}
         counts={counts}
       />
+      <Search searchParam={searchParam} setSearchParam={setSearchParam} />
       <div className="flex flex-col">
-        {applications.length > 0 ? (
+        {filteredApplications.length > 0 ? (
           filteredApplications.map(application => (
             <ApplicationItem
               key={application.id}
               application={application}
               updateCurrentStage={updateCurrentStage}
-              onEdit={onEditHandler}
               onDelete={onDeleteHandler}
             />
           ))
@@ -77,11 +66,13 @@ const Applications = () => {
               src={nodata}
               alt="No data to Display"
             />
-            <p className="text-xl font-medium text-center">
-              Great things never come from comfort zones. Make those
-              applications now!! <br />
-              Let's cause an
-              <span className="text-2xl font-bold"> "ApplicationOverflow"</span>
+            <p className="text-base font-medium text-center">
+              <span className="text-xl font-medium">
+                "No application to show"
+              </span>
+              <br />
+              Great things never come from comfort zones. Send some applications
+              today!
             </p>
           </div>
         )}
