18541e4291ed16d1c10a82dacdc902e741b0f0d1
Noble Obioma <nobioma1@gmail.com>
2020-03-26 07:02:37 +0100

    Create layout

--- client/src/components/Layout/index.jsx
+++ client/src/components/Layout/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Sidebar from '../Sidebar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="container antialiased max-w-4xl mx-auto pt-16 pb-8 px-2">
-      {children}
+    <div className="antialiased mx-auto flex flex-col md:flex-row pt-24 px-2 md:max-w-6xl">
+      <div className="md:w-2/3 mb-5">{children}</div>
+      <div className="md:w-1/3 md:pl-8">
+        <Sidebar />
+      </div>
     </div>
   );
 };
