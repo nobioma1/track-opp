d7f7ee2aba1b03682ddfcc921e8f8010b43fa94a
Noble Obioma <nobioma1@gmail.com>
2020-03-26 22:29:31 +0100

    Create route to get, subscribe and unsubscribe for a user

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,18 +1,58 @@
 const { Router } = require('express');
 
+const redisDB = require('../db/redisClient');
+const checkField = require('../middlewares/checkField');
+
 const router = Router();
 
-router.get('/', (req, res) => {
-  return res.status(200).json({ message: 'up' });
+const FIELDS = [
+  'Software Engineer',
+  'React',
+  'Python',
+  'JavaScript',
+  'FullStack Engineer',
+];
+
+router.get('/fields/:userId', async (req, res, next) => {
+  try {
+    const userSubscriptions = await redisDB.getValue(req.params.userId);
+    const subscriptions = FIELDS.map(field => ({
+      name: field,
+      subscribed: !!userSubscriptions && userSubscriptions.includes(field),
+    }));
+    res.status(200).send({ fields: subscriptions });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/suggestions', (req, res) => {
-  res.end();
+router.post('/subscribe/:userId', checkField, async (req, res, next) => {
+  const { field } = req.body;
+  try {
+    const userSubscriptions = await redisDB.getValue(req.params.userId);
+    if (userSubscriptions.includes(field)) {
+      res.status(200).send({ message: 'Already subscribed' });
+    }
+    const success = await redisDB.setValue(req.params.userId, [
+      ...userSubscriptions,
+      field,
+    ]);
+    res.status(201).send({ name: field, subscribed: !!success });
+  } catch (error) {
+    next(e);
+  }
 });
 
-router.post('/suggestions', (req, res) => {
-  console.log(req.body.info);
-  res.end();
+router.post('/unsubscribe/:userId', checkField, async (req, res, next) => {
+  const { field } = req.body;
+  try {
+    const userSubscriptions = await redisDB.getValue(req.params.userId);
+    const update = userSubscriptions.filter(item => item !== field);
+    await redisDB.setValue(req.params.userId, update);
+    res.status(204).end();
+  } catch (error) {
+    next(e);
+  }
 });
 
 module.exports = router;
