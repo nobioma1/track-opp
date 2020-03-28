481e4e229e75f71035053c672cccadf8d7d42654
Noble Obioma <nobioma1@gmail.com>
2020-03-28 23:20:33 +0100

    Install  puppeter and node-cron, Create function to scrape RemoteOk page for jobs

---
+++ server/scrappers/scrapeROK.js
@@ -0,0 +1,51 @@
+const puppeteer = require('puppeteer');
+
+async function scrapeROK(url) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const jobs = await page.evaluate(() => {
+      function getJobDetails(section) {
+        const jobsEl = section.querySelectorAll('.job');
+        const postings = {};
+
+        jobsEl.forEach(el => {
+          const link = `https://remoteok.io${el.dataset.url}`;
+          const jobTitle = el.querySelector('.company > h2').textContent;
+          const company = el.querySelector('.company .companyLink > h3')
+            .textContent;
+          const tags = Array.from(el.querySelectorAll('.tag')).map(
+            item => item.textContent
+          );
+          const jobId = el.getAttribute('id');
+          const postedAt = el.querySelector('.time').textContent;
+
+          postings[jobId] = { company, link, jobTitle, tags, postedAt };
+        });
+
+        return postings;
+      }
+
+      const sections = document.querySelectorAll('#jobsboard > tbody');
+      let recentJobs, earlierJobs;
+
+      if (sections.length >= 2) {
+        const [recent, earlier] = sections;
+
+        recentJobs = getJobDetails(recent);
+        earlierJobs = getJobDetails(earlier);
+      }
+
+      return { recentJobs, earlierJobs };
+    });
+
+    await browser.close();
+    return jobs;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = scrapeROK;
