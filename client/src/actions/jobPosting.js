63699c51fd4e5181b15ba0fa31ea27faefa7e4c0
Noble Obioma <nobioma1@gmail.com>
2020-04-10 12:37:19 +0100

    Create action creator to get job postings

--- client/src/actions/jobPosting.js
+++ client/src/actions/jobPosting.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 import { API_URL } from '../config';
 import { firebaseAuth } from '../config/firebase';
 
-export const START_SUGGESTIONS_REQUEST = 'START_SUGGESTIONS_REQUEST';
-export const SET_SUGGESTIONS = 'SET_SUGGESTIONS';
-export const END_SUGGESTIONS_REQUEST = 'END_SUGGESTIONS_REQUEST';
+export const START_POSTINGS_REQUEST = 'START_POSTINGS_REQUEST';
+export const SET_POSTINGS = 'SET_POSTINGS';
+export const END_POSTINGS_REQUEST = 'END_POSTINGS_REQUEST';
 export const SET_FIELDS = 'SET_FIELDS';
 
-export const getFields = () => async dispatch => {
+export const getFields = () => async (dispatch) => {
   const userId = firebaseAuth().currentUser.uid;
   try {
     const { data } = await axios.get(`${API_URL}/fields/${userId}`);
@@ -15,29 +15,32 @@ export const getFields = () => async dispatch => {
   } catch (error) {}
 };
 
-export const subscribe = field => async dispatch => {
+export const subscribe = (field) => async (dispatch) => {
   const userId = firebaseAuth().currentUser.uid;
   try {
     await axios.post(`${API_URL}/subscribe/${userId}`, {
       field,
     });
     dispatch(getFields());
+    dispatch(getPostings());
   } catch (error) {}
 };
 
-export const unsubscribe = field => async dispatch => {
+export const unsubscribe = (field) => async (dispatch) => {
   const userId = firebaseAuth().currentUser.uid;
   try {
     await axios.post(`${API_URL}/unsubscribe/${userId}`, {
       field,
     });
     dispatch(getFields());
+    dispatch(getPostings());
   } catch (error) {}
 };
 
-export const postings = () => async dispatch => {
+export const getPostings = () => async (dispatch) => {
   const userId = firebaseAuth().currentUser.uid;
   try {
-    const postings = await axios.get(`${API_URL}/postings/${userId}`);
+    const { data } = await axios.get(`${API_URL}/postings/${userId}`);
+    dispatch({ type: SET_POSTINGS, payload: data });
   } catch (error) {}
 };
