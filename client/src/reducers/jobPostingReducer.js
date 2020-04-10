3d2812709ac7f3e576aed6af2becca58337c8467
Noble Obioma <nobioma1@gmail.com>
2020-04-10 12:36:03 +0100

    Add SET_POSTINGS to update state with postings and source

---
+++ client/src/reducers/jobPostingReducer.js
@@ -0,0 +1,36 @@
+import {
+  START_POSTINGS_REQUEST,
+  SET_FIELDS,
+  END_POSTINGS_REQUEST,
+  SET_POSTINGS,
+} from '../actions/jobPosting';
+
+export const INITIAL_STATE = {
+  isLoading: false,
+  fields: [],
+  subscribedPostings: [],
+  source: {},
+};
+
+const jobPostingReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case START_POSTINGS_REQUEST:
+      return { ...state, isLoading: true };
+    case SET_FIELDS:
+      return { ...state, isLoading: false, fields: action.payload };
+    case END_POSTINGS_REQUEST:
+      return { ...state, isLoading: false };
+    case SET_POSTINGS: {
+      return {
+        ...state,
+        isLoading: false,
+        subscribedPostings: action.payload.subscribedPostings,
+        source: action.payload.source,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default jobPostingReducer;
