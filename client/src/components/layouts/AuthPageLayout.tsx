83c8fc6b0697eea361ddffdc865205a84fa7148c
Noble Obioma <nobioma1@gmail.com>
2021-01-23 19:14:13 +0100

    create authPageLayout to render auth pages router

---
+++ client/src/components/layouts/AuthPageLayout.tsx
@@ -0,0 +1,33 @@
+import { Grid, Box } from '@chakra-ui/react';
+
+import Logo from '../Logo';
+
+const AuthPageLayout: React.FC = ({ children }) => {
+  return (
+    <Grid
+      height="100vh"
+      gridTemplateColumns={{ md: 'repeat(2, 50%)' }}
+      overflow="hidden"
+    >
+      <Box
+        paddingX={5}
+        paddingTop={5}
+        paddingBottom={12}
+        height="100vh"
+        overflowY="scroll"
+      >
+        <Box width={{ lg: '28rem' }} margin={{ lg: '0 auto' }}>
+          <Logo to="/" marginBottom={12} />
+          <Box>{children}</Box>
+        </Box>
+      </Box>
+      <Box
+        height="100%"
+        backgroundColor="purple.600"
+        display={{ base: 'none', md: 'block' }}
+      ></Box>
+    </Grid>
+  );
+};
+
+export default AuthPageLayout;
