669c337e3fca235c77d7e0561f520f15359ea562
Noble Obioma <nobioma1@gmail.com>
2021-01-26 09:39:01 +0100

    pass prop light to change color of logo text to white

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -5,10 +5,11 @@ import LogoImg from '../../assets/logo.svg';
 
 type Props = {
   to: string;
+  light?: boolean;
   marginBottom?: number | { [key: string]: number };
 };
 
-const Logo: React.FC<Props> = ({ to, ...styles }) => {
+const Logo: React.FC<Props> = ({ to, light, ...styles }) => {
   return (
     <Link to={to}>
       <Flex alignItems="center" {...styles}>
@@ -19,7 +20,11 @@ const Logo: React.FC<Props> = ({ to, ...styles }) => {
         >
           <Image src={LogoImg} alt="jobhuntpad" fallback={<div></div>} />
         </Box>
-        <Heading as="h1" fontSize={{ base: 'md', md: 'xl' }}>
+        <Heading
+          as="h1"
+          fontSize={{ base: 'md', md: 'xl' }}
+          color={light ? 'white' : 'purple.700'}
+        >
           JobHuntPad
         </Heading>
       </Flex>
