028733fb27231f4f761efec2a81393146ee854af
Noble Obioma <nobioma1@gmail.com>
2021-01-27 19:30:42 +0100

    add GA

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import { Box } from '@chakra-ui/react';
 
 import ThemeProvider from 'components/Theme';
 import AppRoute from 'routes';
 import { FirebaseContextProvider } from 'contexts/FirebaseContext';
 import { AuthContextProvider } from 'contexts/AuthContext';
+import config from 'config';
 
 const App = () => {
+  ReactGA.initialize(config.googleAnalytics || '');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <ThemeProvider>
       <Router>
