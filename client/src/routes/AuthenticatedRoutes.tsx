f7555d275bcfec2d5f2532bbc1ade1e628d64a56
Noble Obioma <nobioma1@gmail.com>
2021-01-27 19:23:39 +0100

    add applications and profile

--- client/src/routes/AuthenticatedRoutes.tsx
+++ client/src/routes/AuthenticatedRoutes.tsx
@@ -1,20 +1,37 @@
+import { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import { AuthPageLayout } from '../components/layouts';
-import Dashboard from '../components/Dashboard';
+import { AppLayout } from 'layouts';
+import Profile from 'components/Profile';
+import Dashboard from 'components/Dashboard';
+import { Applications } from 'components/Application';
+import { useApplicationsContext } from 'hooks';
 
 const AuthenticatedRoutes = () => {
+  const { getApplications } = useApplicationsContext();
+
+  useEffect(() => {
+    getApplications();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <AuthPageLayout>
+    <AppLayout>
       <Switch>
         <Route path="/dashboard" exact>
           <Dashboard />
+        </Route>
+        <Route path="/applications" exact>
+          <Applications />
+        </Route>
+        <Route path="/profile" exact>
+          <Profile />
         </Route>
         <Route>
           <Redirect to="/dashboard" />
         </Route>
       </Switch>
-    </AuthPageLayout>
+    </AppLayout>
   );
 };
 
