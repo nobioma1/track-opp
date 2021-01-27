f5b44b2ddb1630725c74a425ed80b55e33f2e7e5
Noble Obioma <nobioma1@gmail.com>
2021-01-27 19:23:58 +0100

    export components

---
+++ client/src/components/Application/index.tsx
@@ -0,0 +1,11 @@
+import ApplicationDrawerForm from './ApplicationDrawerForm';
+import RecentApplications from './RecentApplications';
+import Applications from './Applications';
+import NewApplicationButton from './NewApplicationButton';
+
+export {
+  Applications,
+  RecentApplications,
+  ApplicationDrawerForm,
+  NewApplicationButton,
+};
